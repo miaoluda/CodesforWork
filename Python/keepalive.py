c4f0960669ab3186ee3d54a6c8db332a22f1d292
Unnamed <miaoluda@users.noreply.github.com>
2019-05-08 16:52:15 +0800

    Compatible with python2 and python3

--- Python/keepalive.py
+++ Python/keepalive.py
@@ -3,7 +3,20 @@
 
 import sys
 import time
-import thread 
+# Compatible with python2: thread and pytho3: _thread 
+try:
+    import thread 
+except ModuleNotFoundError:
+    import _thread
+
+# Compatible with python2 and python3 
+try:
+    thread = _thread
+    raw_input = input
+except NameError:
+    pass
+
+
 
 starttime = time.time()
 
@@ -16,14 +29,14 @@ def getPassedtime():
         nn = n - m*60.0
         return '{0:d} min {1:.4f} s'.format(m, nn)
 
-
+# keyboard input
 def input_thread(L):
     raw_input()
     L.append(None)
 L = []
 thread.start_new_thread(input_thread, (L,))
 
-print("Trying continuesly exporting to keep an ssh session alive...")
+print("Trying to keep this ssh session alive...")
 
 print("Press Enter to break")
 
@@ -35,5 +48,6 @@ while True:
         break
     n =  time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
     passedtime = '{0:.4f}'.format(time.time() - starttime)
+    # use sys.stdout to stop from printing in new lines.
     sys.stdout.write('\rCurrent time: {0}, {1} has gone...\r'.format(n,getPassedtime()))
     sys.stdout.flush()
